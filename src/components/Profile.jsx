779c5e1f8dd8502183355cee7c8ffbe19f08946a
jul.mihelson@gmail.com <125132083+JulMihelson@users.noreply.github.com>
2023-05-23 17:33:03 +0300

    first task

---
+++ src/components/Profile.jsx
@@ -0,0 +1,39 @@
+// import user from '../../src/user.json'
+import './Profile.css'
+
+export const Profile = ({ username, location, tag, avatar, stats }) => {
+    
+    return (
+        <div class="profile">
+            <div class="description">
+                <img
+                    src={avatar}
+                    alt={username}
+                    class="avatar"
+                />
+                <div class="user-info">
+                <p class="name">{username}</p>
+                <p class="tag">@{tag}</p>
+                    <p class="location">{location}</p>
+                    </div>
+            </div>
+
+            <ul class="stats">
+                <li class="stat-type">
+                    <span class="label">Followers</span>
+                    <span class="quantity">{stats.followers}</span>
+                </li>
+                <li>
+                    <span class="label">Views</span>
+                    <span class="quantity">{stats.views}</span>
+                </li>
+                <li>
+                    <span class="label">Likes</span>
+                    <span class="quantity">{stats.likes}</span>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Profile;

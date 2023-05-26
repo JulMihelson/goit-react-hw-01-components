4ce9cad746c811567ea84f55a1e84ddf291cd0a2
jul.mihelson@gmail.com <125132083+JulMihelson@users.noreply.github.com>
2023-05-26 18:48:01 +0300

    try

--- src/components/FriendStatus.jsx
+++ src/components/FriendStatus.jsx
@@ -1,18 +1,20 @@
 import css from './Friends.module.css';
 
 
+
 export const friendStatus = (element) => {
   const { isOnline } = element;
   if (isOnline) {
     return (
       <svg width="16" height="16">
-  <use xlinkHref="./symbol-defs.svg#icon-smile"></use>
+  <use xlinkHref="../symbol-defs.svg#icon-smile"></use>
 </svg>
     );
   } else {
     return (
       <svg width="16" height="16">
-  <use xlinkHref="./symbol-defs.svg#icon-sad"></use>
+        
+  <use xlinkHref="../symbol-defs.svg#icon-sad"></use>
 </svg>
     );
   }
